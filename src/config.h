5a4eaedb02b670048e0af3ea9f7281cc0f3f19bc
Daniel Roberson <daniel@planethacker.net>
2016-12-28 08:57:50 -0800

    added some banners

--- src/config.h
+++ src/config.h
@@ -1,8 +1,11 @@
 #define VERSION  "0.0.1"
 #define AUTHOR   "Daniel Roberson"
 
-#define LOGFILE  "ssh-honeypot.log"                /* default log location */
+#define LOGFILE  "ssh-honeypot.log"              /* default log location */
+#define PIDFILE  "ssh-honeypot.pid"              /* default pid file location */
+#define PORT     9999                            /* default port */
+#define RSAKEY   "ssh-honeypot.rsa"              /* default RSA key */
+#define BINDADDR "0.0.0.0"                       /* default bind address */
 #define BANNER   "OpenSSH_7.2p2 Ubuntu-4ubuntu2.1" /* fake Ubuntu 16.04 */
-#define PORT     9999                              /* default port */
-#define RSAKEY   "ssh-honeypot.rsa"                /* default RSA key */
-#define BINDADDR "0.0.0.0"                         /* default bind address */
+//#define BANNER  "OpenSSH_6.6.1"                    /* openSUSE 42.1 */
+//#define BANNER  "OpenSSH_6.7p1 Debian-5+deb8u3"    /* Debian 8.6 */
