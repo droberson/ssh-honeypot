a5e89ea38f4021ab83964eff56661ff84c32da47
Daniel Roberson <daniel@planethacker.net>
2017-01-04 18:08:38 +0000

    initial commit

---
+++ scripts/ssh-honeypot-unique-passwords.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ssh-honeypot-unique-passwords.sh -- by Daniel Roberson
+# -- Prints a list of all passwords found in the logs.
+#
+# -- usage: ssh-honeypot-unique-passwords.sh <logfile>
+
+if [ ! $1 ]; then
+  echo "FATAL: no input file!"
+  echo "usage: $0 ssh-honeypot.log"
+  exit 1
+fi
+
+grep "] ssh-honeypot " $1 >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "FATAL: $1 doesn't appear to be a ssh-honeypot log file"
+  exit 1
+fi
+
+grep -v -e " Error exchanging keys: " -e "] ssh-honeypot " $1 |awk {'print $8'} |sort |uniq
