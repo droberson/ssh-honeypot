8d72a7e9e3e7934ab6d8fa49e601d9e3ac90cafc
Daniel Roberson <daniel@planethacker.net>
2022-02-28 07:18:19 -0800

    add TIMEOUT

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 #define PORT         22                          /* default port */
 #define RSAKEY       "ssh-honeypot.rsa"          /* default RSA key */
 #define BINDADDR     "0.0.0.0"                   /* default bind address */
-
+#define TIMEOUT      60                          /* login timeout in seconds */
 #define JSON_LOGFILE "ssh-honeypot.json.log"     /* default json log location */
 #define JSON_SERVER  "127.0.0.1"                 /* send json to this host */
 #define JSON_PORT    55555                       /* send json to this port */
